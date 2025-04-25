1b10e5e0405264e649f6d5cf8c2bf57cf823d2db
KarlaNunes <karla.julyana1@gmail.com>
2025-04-25 15:36:25 -0300

    feat: create new task card

---
+++ src/components/new-task-card.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Plus } from "lucide-react";
+
+export function NewTaskCard() {
+  return (
+    <div
+      className="flex flex-col gap-4 p-4 bg-white rounded-lg shadow-md cursor-pointer border border-gray-100 max-w-[800px] w-full"
+    >
+        <h3 className="text-md font-semibold">Nova Tarefa</h3>
+        <input 
+            type="text"
+            placeholder="Título da tarefa"
+            className="p-2 text-sm border border-gray-300 rounded"
+        />
+
+        <textarea
+            placeholder="Descrição (opcional)"
+            className="p-2 text-sm border border-gray-300 rounded"
+            rows={3}
+        />
+
+        <button
+            className="px-4 py-2 mt-3 flex items-center justify-center gap-2 text-sm text-white font-bold bg-black rounded hover:bg-gray-800 cursor-pointer"
+            onClick={() => alert("Tarefa criada!")}
+        >
+            <Plus 
+              className="inline mr-2" 
+              size={16}  
+            />
+            Adicionar Tarefa
+        </button>
+    </div>
+  );
+}
