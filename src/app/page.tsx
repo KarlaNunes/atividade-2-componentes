72c6efc17dfd577631a62c9b4c09299b2637f9a2
KarlaNunes <karla.julyana1@gmail.com>
2025-04-30 16:02:38 -0300

    feat: add new task using state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
+"use client";
+
+import { Task } from "@/@types/task";
 import { NewTaskCard } from "@/components/new-task-card";
 import { TaskCard } from "@/components/task-card";
+import { useState } from "react";
+
+const tasksData: Task[] = [
+  {
+    id: 1,
+    title: "Exemplo de Tarefa 1",
+    description: "Esta é uma tarefa de exemplo. Você pode marcar como concluída, editar ou excluir.",
+    completed: false,
+  },
+]
 
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>(tasksData);
+
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)] bg-white">
       <h1 className="text-3xl font-bold">Lista de Tarefas</h1>
-      <NewTaskCard />  
+      <NewTaskCard 
+        addTask={setTasks}
+      />  
 
       <div className="grid grid-cols-2 gap-4 max-w-[800px] w-full">
-        <TaskCard 
-          title="Exemplo de Tarefa "
-          description="Esta é uma tarefa de exemplo. Você pode marcar como concluída, editar ou excluir."
-        />
-
-        <TaskCard 
-          title="teste"
-          description="teste"
-        />
+        {tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            title={task.title}
+            description={task.description}
+          />
+        ))}
       </div>     
     </div>
   );
--- src/components/new-task-card.tsx
+++ src/components/new-task-card.tsx
@@ -1,8 +1,28 @@
 "use client";
 
+import { Task } from "@/@types/task";
 import { Plus } from "lucide-react";
+import { Dispatch, SetStateAction } from "react";
 
-export function NewTaskCard() {
+interface NewTaskCardProps {
+  addTask: Dispatch<SetStateAction<Task[]>>
+}
+
+export function NewTaskCard({
+  addTask,
+}: NewTaskCardProps) {
+  const handleClick = () => {
+    addTask(prevTasks => [
+      ...prevTasks,
+      {
+        id: prevTasks.length + 1,
+        title: "Nova Tarefa",
+        description: "Descrição da nova tarefa",
+        completed: false,
+      },
+    ]);
+  };
+
   return (
     <div
       className="flex flex-col gap-4 p-4 bg-white rounded-lg shadow-md cursor-pointer border border-gray-100 max-w-[800px] w-full"
@@ -22,7 +42,7 @@ export function NewTaskCard() {
 
         <button
             className="px-4 py-2 mt-3 flex items-center justify-center gap-2 text-sm text-white font-bold bg-black rounded hover:bg-gray-800 cursor-pointer"
-            onClick={() => alert("Tarefa criada!")}
+            onClick={handleClick}
         >
             <Plus 
               className="inline mr-2" 
